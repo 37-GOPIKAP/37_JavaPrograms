d4e635a67da1a63d500eaa60afc8990d066ae958
GOPIKA8299 <62229869+GOPIKA8299@users.noreply.github.com>
2021-06-14 21:14:25 +0530

    prgm no 5

---
+++ User_input/ArrayLargest.java
@@ -0,0 +1,28 @@
+//Java program to display the largest element in an array
+ import java.util.Scanner;
+class ArrayLargest
+{
+public static void main(String[]args)
+{
+int i,n;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no of elements of an array");
+n=sc.nextInt();
+int a[]=new int[n];
+int rev[]=new int[n];
+System.out.println("enter the elements of an array");
+for(i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+int max=a[0];
+for(i=0;i<n;i++)
+{
+if(max<a[i])
+{
+max=a[i];
+}
+}
+System.out.println("Largest element in the array elements:\n"+max);
+}
+}
