0fb1ac9ffd9713aca1f1278e8d1f56724767bb8c
GOPIKA8299 <62229869+GOPIKA8299@users.noreply.github.com>
2021-06-23 19:38:11 +0530

    program no 1

--- CO1/productprice.java
+++ CO1/productprice.java
@@ -2,40 +2,32 @@ class Product{
 String pcode;
 String pname;
 double price;
-
-String lesscost(Product p1, Product p2){
-	if(p1.price<p2.price)
-		return p1.pname;
-	if(p1.price<price)
-		return p1.pname;
-	if(p2.price<p1.price)
-		return p2.pname;
-	if(p2.price<price)
-		return p2.pname;
-	else
-		return pname;
-}
-void setClass(String pc,String pn ,double p)
+Product(String pc, String pn, double p)
 {
-pcode=pc;
-pname=pn;
-price=p;
-} 
+this.pcode=pc;
+this.pname=pn;
+this.price=p;
+}
+String lowcost(Product p1, Product p2){
+if(p1.price<p2.price && p1.price<price)
+return p1.pname;
+if(p2.price<p1.price && p2.price<price)
+return p2.pname;
+else
+return pname ;
+}
 }
 class productprice{
 public static void main(String args[])
 {
-Product p1=new Product();
-Product p2=new Product();
-Product p3=new Product();
+Product p1=new Product("001", "MI laptop", 30000);
+Product p2=new Product("002", "Redmi 9i", 9000);
+Product p3=new Product("003", "SmartBand", 2000);
 String pnname;
 
-p1.setClass("001", "MI laptop", 30000);
-p2.setClass("002", "Redmi 9i", 9000);
-p3.setClass("003", "SmartBand", 2000);
 
-pnname=p3.(p1,p2);
-System.out.println("Lowest price is" +pnname);
+pnname=p3.lowcost(p1,p2);
+System.out.println("Lowest price is  for " +pnname );
 
 }
 }
